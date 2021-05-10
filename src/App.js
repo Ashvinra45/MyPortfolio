0c28840f713473bdadddcb0d5ebc98fef4aa3f19
Ashvin R@Ashvin.com <you@example.com>
2021-05-10 17:28:50 +0530

    add MyCarousel and scroll down components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import MyCarousel from './components/carousal/MyCarousel';
 import MyNavbar from './components/navbar/MyNavbar';
+import TitleMsg from './components/title-msg/TitleMsg';
 
 const App = ()=> {
   return (
     <div className="App">
       <MyNavbar />
+      <MyCarousel />
+      <TitleMsg />
     </div>
   );
 }
